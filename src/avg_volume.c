e7dc7b4e596adf345a70a70e11a167a051e5368e
99003721 <78872643+99003721@users.noreply.github.com>
2021-02-25 14:31:37 +0530

    Add files via upload
    Commit by 99003721

---
+++ src/avg_volume.c
@@ -0,0 +1,27 @@
+
+#include "Box_Main.h"
+
+
+struct Box_details{
+    int id; char name[50];char color[50];
+    float Box_breadth,Box_weight;
+    float Box_length;float Box_height;float volume;
+    
+};
+
+
+int Box_Avg_Volume(Box_details);
+
+int Box_Avg_Volume(Box_length,Box_breadth,Box_height,Box_weight)
+    float sum =ptr[i].volume;
+      struct Box_details *ptr;
+    int i, noOfRecords;
+    int  n, highest = 0;
+        {
+            ptr=initial;
+             
+             sum=(sum + ptr[i].volume)/noOfRecords;
+            // printf("%f\n",sum);
+             ptr++;
+             return sum;
+}
