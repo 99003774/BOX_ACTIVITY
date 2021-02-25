846028e7a8c33fb8b8e81854aee707b9f001e9ad
99003763 <78864900+99003763@users.noreply.github.com>
2021-02-25 09:54:42 +0530

    Add files via upload
    Commited By Rajat

---
+++ Inc/Box_Main.h
@@ -0,0 +1,32 @@
+/**
+ * @file Box_Main.h
+ * @author Rajat Kumar Singh
+ * @brief Box Volume and Find the Box ID is AVilable or not
+ * @version 0.1
+ * @date 2021-02-25
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+#include<string.h>
+
+
+/**
+ * @brief Box Volume and Find the Box is AVilable or not
+ * @param char, int
+ */
+int Box_Volume();
+
+/**
+ * @brief  Find the Box ID  is AVilable or not
+ 
+ */
+int Box_FindBox();
+/**
+ * @brief Count the color which Color user enter
+ * 
+ * 
+ */
+
+int Count_Color(box_color);
